84168aa3d66b66b87c60a290e851a6553a5fdca4
GerritRiesch94 <gerrit.riesch@live.de>
2023-03-29 10:59:21 +0200

    initial setup

---
+++ src/main/java/com/example/example/api/AppController.java
@@ -0,0 +1,29 @@
+package com.example.example.api;
+
+import com.example.example.service.AppService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/data")
+public class AppController {
+
+    private final AppService appService;
+
+    public AppController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<String>> getData(@PathVariable String id, @RequestParam Integer skip, @RequestParam Integer top) {
+        // should represent a database service, api call, etc.
+        var data = this.appService.searchForIdAndPaginate(id, skip, top);
+
+        if (!data.isEmpty()) {
+            return ResponseEntity.ok(data);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/example/service/AppService.java
@@ -0,0 +1,15 @@
+package com.example.example.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class AppService {
+
+    public List<String> searchForIdAndPaginate(String id, Integer skip, Integer top) {
+        return Objects.equals(id, "0") ? List.of("result 1", "result 2") : Collections.emptyList();
+    }
+}
