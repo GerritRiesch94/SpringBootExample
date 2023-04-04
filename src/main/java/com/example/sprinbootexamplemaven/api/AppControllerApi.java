14065864fbd68f4e6c60d62204a75cdedce960cf
Gerrit Riesch <gerrit.riesch@live.de>
2023-04-04 18:00:10 +0200

    docs(api doc): refactor api doc for app controller

--- src/main/java/com/example/sprinbootexamplemaven/api/AppControllerApi.java
+++ src/main/java/com/example/sprinbootexamplemaven/api/AppControllerApi.java
@@ -1,7 +1,9 @@
 package com.example.sprinbootexamplemaven.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,27 @@ import java.util.List;
 @RequestMapping("/api/data")
 public interface AppControllerApi {
 
-    @Operation(summary = "Get a list of strings")
+    @Operation(description = "Get a list of strings")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "return list of string for id and pagination params"),
-            @ApiResponse(responseCode = "204", description = "empty list found for given parameter", content = @Content),
-            @ApiResponse(responseCode = "404", description = "no resources found for given id", content = @Content)
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "return list of strings for id and pagination params",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = String.class)
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "empty list found for given parameter",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "no resources found for given id",
+                    content = @Content
+            )
     })
     @GetMapping("/{id}")
     ResponseEntity<List<String>> getData(@PathVariable String id, @RequestParam Integer skip, @RequestParam Integer top);
